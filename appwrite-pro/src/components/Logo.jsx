479ebbcbdc1f3ff5be8b62f18774d05fee22ac11
Rehan-Pathan <rehanpathanofficial@gmail.com>
2024-12-01 17:55:17 +0530

    .

--- appwrite-pro/src/components/Logo.jsx
+++ appwrite-pro/src/components/Logo.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
+import logoImage from '../assets/logo.jpg';  // Adjust the relative path if necessary
 
 function Logo({ width = '100px' }) {
   return (
     <div>
       <img 
-        src="/path/to/your/logo.png"  // Replace with the actual path to your image
+        src={logoImage}  // Replace with the actual path to your image
         alt="Logo"
         style={{ width: width }}  // You can dynamically adjust the width of the logo using props
       />
